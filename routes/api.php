17215893a2664254f69967339d906c65997039e5
AJ <edulife1999@gmail.com>
2026-09-03 15:59:25 +0600

    changes

--- routes/api.php
+++ routes/api.php
@@ -10,13 +10,13 @@ Route::prefix('v1')->group(function () {
 
     Route::prefix('auth')->group(function () {
 
-        Route::post('/register', [AuthController::class, 'register']);
-        Route::post('/login', [AuthController::class, 'login']);
-        Route::post('/forgot-password',[AuthController::class, 'forgotPassword']);
+        Route::post('register', [AuthController::class, 'register']);
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('forgot-password',[AuthController::class, 'forgotPassword']);
 
         Route::middleware('auth:sanctum')->group(function () {
 
-            Route::post('/logout',[AuthController::class, 'logout']);
+            Route::post('logout',[AuthController::class, 'logout']);
 
         });
     });
